3ca68387c5824b23a7065d96e7af71def693af8f
AryanSingh077 <aryanbaghi076@gmail.com>
2025-08-16 17:10:29 +0530

    had done basics conversion and operation

---
+++ learning.js
@@ -0,0 +1,49 @@
+//some conversions
+
+// const score = "Aryan"
+// const valueInNum = Boolean(score)
+// console.log(score);
+// console.log(valueInNum);
+// console.log(typeof(score));
+// console.log(typeof(valueInNum));
+
+// let value = 33
+// let negvalue = -value
+// console.log(value);
+// console.log(negvalue);
+
+
+// let value = "happy" if  we will take string then negvalue will show NaN(obvious);
+// let negvalue = -value
+// console.log(value);
+// console.log(negvalue);
+
+// some operations
+
+// console.log(2*3); //and 
+// console.log(2**3); //this for power(hehehheehehehe)
+
+// let as1 = "I'm";
+// let as2 = " Aryan";
+// let as3 = " Singh";
+
+// let as4 = as1 + as2 + as3;
+// console.log(as4); this is basiczzz brooo;
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + 2 + "2");
+// console.log("1" + 2 + 2); //maybe, cause its takes sting datatype as priority(maybe) and it checks form left to right;
+
+// let badmosi = 699;
+// ++badmosi;
+// console.log(badmosi);// simply it increase value by 1;
+
+// let x = "6";
+// let y = ++x;
+
+let x = "6";
+let y = x++;
+
+console.log(x);
+console.log(y);
