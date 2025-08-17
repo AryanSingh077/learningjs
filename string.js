bb43a97910435e873691623a52bc9b7838ef0b11
AryanSingh077 <aryanbaghi076@gmail.com>
2025-08-17 21:37:00 +0530

    basics of string

---
+++ string.js
@@ -0,0 +1,37 @@
+// const name = "Aryan";
+// const repoCount = 9;
+
+// console.log(`Hello my is ${name} and my recpcount is ${repoCount}`);
+
+//i can also write this as..
+
+// const userName = new String('AryanSingh')
+
+// console.log(userName[0]);
+// console.log(userName[1]);
+// console.log(userName[2]);
+// console.log(userName[3]);
+// console.log(userName[4]);
+// console.log(userName.__proto__);
+// console.log(userName.length);
+// console.log(userName.toUpperCase());
+// console.log(userName.charAt('3'));
+// console.log(userName.indexOf('r'));
+
+//Badmosi on topppp;]
+
+// const newString = userName.substring(0,7);
+// const anoString = userName.slice(-8,3);
+// console.log(newString);
+// console.log(anoString);
+
+// const Baddmosi = "    badmos    ";
+// console.log(Baddmosi);
+// console.log(Baddmosi.trim());
+
+// const url = "https://aryan.com/aryan%20singh"
+// console.log(url.replace('%20', '-'))
+// console.log(url.includes('singh'))
+
+const chuha = new String('Cosmic-Chuha')
+console.log(chuha.split('-'));
