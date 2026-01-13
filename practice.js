f9f14fd033d1fc406c108dfed96b28a566383296
Aryan Singh <aryanbaghi076@gmail.com>
2026-01-13 18:15:58 +0530

    nothin

--- practice.js
+++
@@ -1,9 +0,0 @@
-const btn = document.querySelector("#btn");
-const output = document.querySelector("#output");
-let isWhite = true;
-
-btn.addEventListener("click", () => {
-    output.textContent = "Button clicked";
-    document.body.style.backgroundColor = isWhite ? "black" : "white";
-    isWhite = !isWhite;
-})
